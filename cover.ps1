38488ed70113edd50c0b2c2cc3ed0689406c050c
blushingpenguin <mark@blushingpenguin.com>
2019-07-17 08:01:55 +0100

    packaging / build options

--- cover.ps1
+++ cover.ps1
@@ -18,6 +18,7 @@ function script:exec {
 		[Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
 		[Parameter(Position=1,Mandatory=0)][string]$errorMessage = ("Error executing command: {0}" -f $cmd)
     )
+    # write-host $cmd
 	& $cmd
 	if ($lastexitcode -ne 0)
 	{
