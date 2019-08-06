a3b903eb397986bc4230aeee8827810485f97eba
blushingpenguin <mark@blushingpenguin.com>
2019-08-06 07:40:16 +0100

    change version number format to timestamp

---
+++ src/MongoMigrations.Test/M20190718162300_TestCollectionMigration.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace MongoMigrations.Test
+{
+    class M20190718162300_TestCollectionMigration : CollectionMigration
+    {
+        public bool Throw { get; set; }
+
+        public M20190718162300_TestCollectionMigration() :
+            this(false)
+        {
+        }
+
+        public M20190718162300_TestCollectionMigration(bool throw_) :
+            base("TestDocs", "A test collection migration")
+        {
+            Throw = throw_;
+        }
+
+        public override Task<bool> UpdateDocumentAsync(
+            IMongoCollection<BsonDocument> collection, BsonDocument document)
+        {
+            if (Throw)
+            {
+                throw new Exception("Update failure");
+            }
+            document.Set("AnotherNewField", "bob");
+            return Task.FromResult(true);
+        }
+    }
+}
